d7a712977dcf0a10b3483cfdd8c384ee45310f3e
iremlaya <iremkaya1999@hotmail.com>
2019-08-05 17:41:20 +0300

    adding to chrome

---
+++ build/precache-manifest.418751be8fec878df78f3fa54750d72c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57fc8db473169c232745c941072a4fdf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6db84348816eee068a53",
+    "url": "/static/css/main.dcedd5ab.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "6db84348816eee068a53",
+    "url": "/static/js/main.67bba4c6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e2447fadaa4b4627583557a94f2971da",
+    "url": "/static/media/refresh.e2447fad.svg"
+  }
+]);
